11496323720d11ce6850a717f8c6032d68771b03
dcDalin <mcdalinoluoch@gmail.com>
2021-11-30 21:23:03 +0300

    Dev branch

--- pages/tours.tsx
+++ pages/tours.tsx
@@ -9,7 +9,7 @@ const Tours: NextPage = () => {
     <>
       <NextSeo title="Tours" description="A short description goes here." />
       <AppLayout>
-        <p className="text-4xl">Tours</p>
+        <p className="text-4xl">Tours page</p>
       </AppLayout>
     </>
   );
