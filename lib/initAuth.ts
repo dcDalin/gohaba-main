cc92d84507a524a4622a070f4455a22ad8e0eb06
dcDalin <mcdalinoluoch@gmail.com>
2021-12-01 01:45:59 +0300

    Update private key parse

--- lib/initAuth.ts
+++ lib/initAuth.ts
@@ -13,7 +13,9 @@ const initAuth = () => {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
         // The private key must not be accesssible on the client side.
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          : undefined
       },
       databaseURL: 'https://dev-gohaba-auth.firebaseio.com'
     },
